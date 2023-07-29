09f393f099f099fe3c05243191bb18794ad6452d
Matt Knight <mattnite@proton.me>
2023-07-28 18:27:33 -0700

    export to C ABI

--- src/main.zig
+++ src/main.zig
@@ -2,10 +2,6 @@ const std = @import("std");
 const builtin = @import("builtin");
 const testing = std.testing;
 
-pub fn get_optimization() []const u8 {
+export fn get_optimization() [*:0]const u8 {
     return @tagName(builtin.mode);
-}
-
-test get_optimization {
-    try testing.expectEqualStrings("Debug", get_optimization());
 }
